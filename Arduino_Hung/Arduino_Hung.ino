30d5b6815b1712b2ada3f29c362c94f1bcb7a87d
Hung <thaihuntv2014@gmail.com>
2019-12-21 16:49:17 +0700

    up code

---
+++ Arduino_Hung/Arduino_Hung.ino
@@ -0,0 +1,269 @@
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
+#include <SerialCommand.h>  // Thêm vào sketch thư viện Serial Command
+#include <DHT.h> // Gọi thư viện DHT22
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+RCSwitch FanSwitch = RCSwitch();
+const int DHTPIN = 13; //Đọc dữ liệu từ DHT22 ở chân D2 trên mạch Arduino
+const int DHTTYPE = DHT22; //Khai báo loại cảm biến, có 2 loại là DHT11 và DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+const byte RX = 3; //11
+const byte TX = 2;  //10        // Chân 2 được dùng làm chân TX
+
+SoftwareSerial mySerial = SoftwareSerial(RX, TX);
+SerialCommand sCmd(mySerial); // Khai báo biến sử dụng thư viện Serial Command
+
+//const int fanOffPin = 4;
+//const int fanOnPin = 5;
+
+const int speed1Pin = 6;
+const int speed2Pin = 7;
+const int speed3Pin = 8;
+
+//const int lightControlPin = 11;
+const int lightStatusPin = 9;// Cổng số 9 bị lỗi không set được OUTPUT
+
+//const int ATControlPin = 12;
+const int ATStatusPin = 10;
+
+
+void setup() {
+  //Khởi tạo Serial ở baudrate 115200 để debug ở serial monitor
+  Serial.begin(115200);
+
+  mySwitch.enableTransmit(5);
+  //FanSwitch.enableTransmit(11);
+  onClickButtonOffFan();
+  dht.begin(); // Khởi động cảm biến nhiệt
+  //Khởi tạo Serial ở baudrate 115200 cho cổng Serial thứ hai, dùng cho việc kết nối với ESP8266
+  mySerial.begin(57600);
+
+
+  pinMode (lightStatusPin, INPUT);
+  pinMode (ATStatusPin, INPUT);
+  pinMode(speed1Pin, INPUT);
+  pinMode(speed2Pin, INPUT);
+  pinMode(speed3Pin, INPUT);
+
+  // digitalWrite(fanOffPin, HIGH);
+  Serial.println("Da san sang nhan lenh");
+  //Xử lý thực hiện lệnh từ server
+  sCmd.addCommand("CONTROL", processControl);
+
+}
+unsigned long chuky = 0;
+const unsigned long TIME_RETRIEVE_DATA = 30000UL; //Cứ sau 2000ms = 5s thì chu kỳ lặp lại
+
+void loop() {
+  if (millis() - chuky > TIME_RETRIEVE_DATA) {
+    chuky = millis();
+    sendStatusLight();
+    delay(20);
+    sendStatusFan();
+    delay(20);
+    sendStatusAptomat();
+    delay(20);
+    sendTempHumi();
+    delay(20);
+  }
+  sCmd.readSerial();
+  delay(500);
+}
+
+void processControl() {
+  char *json = sCmd.next();
+  Serial.println(json);
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(json);
+  if (root.containsKey("light")) {
+    onChangeLight();
+  }
+  if (root.containsKey("apt")) {
+    onChangeAptomat();
+  }
+  if (root.containsKey("fan_on")) {
+    onClickButtonOnFan();
+  }
+  if (root.containsKey("fan_off")) {
+    onClickButtonOffFan();
+  }
+  if (root.containsKey("init")) {
+    Serial.println("Init -> get all value of device");
+    sendStatusLight();
+    delay(20);
+    sendStatusFan();
+    delay(20);
+    sendStatusAptomat();
+    delay(20);
+    sendTempHumi();
+    delay(20);
+  }
+
+
+}
+
+// *****************************************
+//   Cảnh báo nhiệt độ bếp
+//******************************************
+
+void nhietdo_bep()
+{
+  float h = dht.readHumidity(); //Đọc độ ẩm
+  float t = dht.readTemperature(); //Đọc nhiệt độ
+
+  Serial.print("Nhiet do: ");
+  Serial.println(t); //Xuất nhiệt độ
+  Serial.print("Do am: ");
+  Serial.print(h); //Xuất độ ẩm
+  Serial.println("%");
+  delay(100);
+}
+
+// *****************************************
+//   Tắt quạt trần tầng một
+// ******************************************
+
+void onClickButtonOffFan() {
+  unsigned long address = 0b0101010101010100;
+  Serial.println("ACTION -> OFF FAN");
+  mySwitch.send((address << 8) | 0b00001100, 24);
+  delay(80);
+  mySwitch.send((address << 8) | 0b00000000, 24);
+
+  sendStatusFan();
+}
+// *****************************************
+//   Bật quạt trần tầng một
+//******************************************
+
+void onClickButtonOnFan() {
+  unsigned long address = 0b0101010101010100;
+  Serial.println("ACTION -> ON FAN");
+  mySwitch.send((address << 8) | 0b00000011, 24);
+  delay(80);
+  mySwitch.send((address << 8) | 0b00000000, 24);
+
+  sendStatusFan();
+}
+// *****************************************
+//   Bật/tắt đèn P.khách01
+//******************************************
+
+void onChangeLight() {
+  unsigned long address = 0b0101010101010101;
+  Serial.println("ACTION -> CHANGE LIGHT");
+
+  mySwitch.send((address << 8) | 0b00110000, 24);
+  delay(80);
+  mySwitch.send((address << 8) | 0b00000000, 24);
+  sendStatusLight();
+}
+// *****************************************
+//   Bật/tắt Atomat tổng
+//******************************************
+void onChangeAptomat() {
+  unsigned long address = 0b0101010101010101;
+  Serial.println("ACTION -> CHANGE APTOMAT");
+  mySwitch.send((address << 8) | 0b00000011, 24);
+  delay(80);
+  mySwitch.send((address << 8) | 0b00000000, 24);
+  sendStatusAptomat();
+}
+
+void sendTempHumi() {
+  float h = dht.readHumidity(); //Đọc độ ẩm
+  float t = dht.readTemperature(); //Đọc nhiệt độ
+  float co = random(30, 70);
+
+  StaticJsonBuffer<200> jsonBuffer2;
+  JsonObject& root = jsonBuffer2.createObject();
+  root["temp"] = t;
+  root["humi"] = h;
+  root["co"] = co;
+
+  //in ra cổng software serial để ESP8266 nhận
+  mySerial.print("DATA");   //gửi tên lệnh
+  mySerial.print('\r');           // gửi \r
+  root.printTo(mySerial); //gửi chuỗi JSON
+  mySerial.print('\r');           // gửi \r
+  Serial.print("TEMP_HUMI_CO: ");
+  root.printTo(Serial);
+  Serial.print('\n');
+
+}
+// *****************************************
+//   Đọc trạng thái đèn P.khách01
+//******************************************
+
+void sendStatusLight() {
+  int lightStatus = !digitalRead(lightStatusPin);
+   Serial.println("Value light: " + digitalRead(lightStatusPin));
+  //Send data to ESP8266
+  StaticJsonBuffer<200> jsonBuffer2;
+  JsonObject& root2 = jsonBuffer2.createObject();
+  root2["light"] = lightStatus;
+  //in ra cổng software serial để ESP8266 nhận
+  mySerial.print("DATA");   //gửi tên lệnh
+  mySerial.print('\r');           // gửi \r
+  root2.printTo(mySerial); //gửi chuỗi JSON
+  mySerial.print('\r');           // gửi \r
+  Serial.print("LIGHT: ");
+  root2.printTo(Serial);
+  Serial.print('\n');
+}
+// *****************************************
+//   Đọc trạng thái quạt trần tầng một.
+//******************************************
+
+void sendStatusFan() {
+  // read the state of the level fan value:
+  int speed1Status = !digitalRead(speed1Pin);
+  int speed2Status = !digitalRead(speed2Pin);
+  int speed3Status = !digitalRead(speed3Pin);
+  //Send data to ESP8266
+  StaticJsonBuffer<200> jsonBuffer2;
+  JsonObject& root2 = jsonBuffer2.createObject();
+  if(speed1Status == 1){
+    root2["fan"] = 3;
+  }else if (speed2Status == 1){
+    root2["fan"] = 2;
+  }else if (speed3Status == 1){
+    root2["fan"] = 1;
+  }else{
+    root2["fan"] = 0;
+  }
+  //in ra cổng software serial để ESP8266 nhận
+  mySerial.print("DATA");   //gửi tên lệnh
+  mySerial.print('\r');           // gửi \r
+  root2.printTo(mySerial); //gửi chuỗi JSON
+  mySerial.print('\r');           // gửi \r
+
+  //in ra Serial Arduino để debug
+  Serial.print("FAN:  ");
+  root2.printTo(Serial); //Xuống dòng
+  Serial.print('\n');
+}
+// *****************************************
+//   Đọc trạng thái Atomat tổng
+//******************************************
+
+void sendStatusAptomat() {
+  int statusAT = !digitalRead(ATStatusPin);
+  //Send data to ESP8266
+  StaticJsonBuffer<200> jsonBuffer2;
+  JsonObject& root2 = jsonBuffer2.createObject();
+  root2["apt"] = statusAT;
+  //in ra cổng software serial để ESP8266 nhận
+  mySerial.print("DATA");   //gửi tên lệnh
+  mySerial.print('\r');           // gửi \r
+  root2.printTo(mySerial); //gửi chuỗi JSON
+  mySerial.print('\r');           // gửi \r
+
+  //in ra Serial Arduino để debug
+  Serial.print("APTOMAT: ");
+  root2.printTo(Serial); //Xuống dòng
+  Serial.print('\n');
+}
---
+++ ESP8266_Hung/ESP8266_Hung.ino
@@ -0,0 +1,127 @@
+#define TCP_MSS whatever
+#define LWIP_IPV6 whatever
+#define LWIP_FEATURES whatever
+#define LWIP_OPEN_SRC whatever
+
+//Thư viện Wifi manager
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include "WiFiManager.h" 
+//
+#include <SoftwareSerial.h>
+#include <SocketIOClient.h>
+#include <SerialCommand.h>
+
+//include thư viện để kiểm tra free RAM trên con esp8266
+extern "C" {
+#include "user_interface.h"
+}
+
+const byte RX = 5;
+const byte TX = 4;
+
+SoftwareSerial mySerial(RX, TX, false, 256);
+SerialCommand sCmd(mySerial); // Khai báo biến sử dụng thư viện Serial Command
+
+//Cài đặt Socket client
+SocketIOClient client;
+char host[] = "smarthome116.herokuapp.com";  //Địa chỉ IP dịch vụ, hãy thay đổi nó theo địa chỉ IP Socket server của bạn.
+//peaceful-lake-63408.herokuapp.com,smarthome116.herokuapp.com
+int port = 80;  //80,3484                  //Cổng dịch vụ socket server do chúng ta tạo!
+char namespace_esp8266[] = "esp8266";   //Thêm Arduino!
+
+//từ khóa extern: dùng để #include các biến toàn cục ở một số thư viện khác. Trong thư viện SocketIOClient có hai biến toàn cục
+// mà chúng ta cần quan tâm đó là
+// RID: Tên hàm (tên sự kiện
+// Rfull: Danh sách biến (được đóng gói lại là chuối JSON)
+extern String RID;
+extern String Rfull;
+
+//Khi kết nối wifi thất bại
+void configModeCallback (WiFiManager *myWiFiManager)
+{
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
+//Gửi dữ liệu về cho Socket Server
+void sendSocketServer(String command)
+{
+  char *json = sCmd.next();
+  client.send(command, (String) json);
+
+  //In ra serial monitor để debug
+  Serial.print(command);
+  Serial.print(' ');
+  Serial.println(json);
+}
+
+// Cài đặt thông số ban đầu
+void setup()
+{
+  Serial.begin(115200);
+  mySerial.begin(57600); //Bật software serial để giao tiếp với Arduino, nhớ để baudrate trùng với software serial trên mạch arduino
+  delay(10);
+
+  //Việc đầu tiên cần làm là kết nối vào mạng Wifi
+  Serial.print("Ket noi vao mang ");
+  //Khai báo wifiManager thuộc class WiFiManager, được quy định trong file WiFiManager.h
+  WiFiManager wifiManager;
+  //có thểreset các cài đặt cũ bằng cách gọi hàm:
+  //wifiManager.resetSettings();
+
+  //Cài đặt callback, khi kết nối với wifi cũ thất bại, thiết bị sẽ gọi hàm callback
+  //và khởi động chế độ AP với SSID được cài tự động là "ESP+chipID"
+  wifiManager.setAPCallback(configModeCallback);
+  if (!wifiManager.autoConnect()) {
+    Serial.println(F("Ket noi den wifi that bai!"));
+    //Nếu kết nối thất bại, thử kết nối lại bằng cách reset thiết bị
+    ESP.reset();
+    delay(1000);
+  }
+
+  Serial.println(F("Da ket noi WiFi"));
+
+  if (client.connect(host, port, namespace_esp8266)) {
+     Serial.println("Socket.IO connected !");
+  }else{
+     Serial.println("Socket.IO not connected.");
+     return;
+  }
+
+  sCmd.addDefaultHandler(sendSocketServer); //Lệnh nào đi qua nó cũng bắt hết, rồi chuyển xuống hàm sendSocketServer!
+  Serial.println("Da san sang nhan lenh");
+
+}
+
+void loop()
+{
+  //Kết nối lại server socket!
+  if (!client.connected()) {
+    Serial.println(F("Reconnect !!!"));
+    client.connect(host, port, namespace_esp8266);
+    return;
+  }
+  //Khi bắt được bất kỳ sự kiện nào thì chúng ta có hai tham số:
+  //  +RID: Tên sự kiện
+  //  +RFull: Danh sách tham số được nén thành chuỗi JSON!
+  if (client.monitor())
+  {
+    //gửi tên sự kiện cho adruino xử lý
+    mySerial.print(RID);
+    mySerial.print('\r');
+    mySerial.print(Rfull);
+    mySerial.print('\r');
+
+
+    //in ra serial monitor
+    Serial.print(RID);
+    Serial.print(' ');
+    Serial.println(Rfull);
+
+  }
+
+  sCmd.readSerial();
+}
